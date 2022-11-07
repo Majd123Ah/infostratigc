4af6ec15e550ff8ce19e06d0753a4fb5790e24e7
Majd123Ah <majd91332@gmail.com>
2022-11-07 00:38:21 -0800

    first page completed

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -32,19 +32,19 @@ const Navbar = () => {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
             <li className="nav-item" >
-             <Link className="nav-link" to="Header" spy={true} smooth={true} offset={50} duration={500} >Home</Link>
+             <Link className="nav-link" to="Header" spy={true} smooth={true} offset={-125} duration={500} >Home</Link>
             </li>
             <li className="nav-item">
-             <Link className="nav-link" to="service" spy={true} smooth={true} offset={50} duration={500} >Services</Link>
+             <Link className="nav-link" to="service" spy={true} smooth={true} offset={-125} duration={500} >Services</Link>
             </li>
             <li className="nav-item">
-             <Link className="nav-link" to="project" spy={true} smooth={true} offset={50} duration={500} >Project</Link>
+             <Link className="nav-link" to="project" spy={true} smooth={true} offset={-125} duration={500} >Project</Link>
             </li>
             <li className="nav-item">
-             <Link className="nav-link" to="ContactUs" spy={true} smooth={true} offset={50} duration={500} >Contact us</Link>
+             <Link className="nav-link" to="ContactUs" spy={true} smooth={true} offset={-125} duration={500} >Contact us</Link>
             </li>
             <li className="nav-item">
-             <Link className="nav-link" to="Footer" spy={true} smooth={true} offset={50} duration={500} >About us</Link>
+             <Link className="nav-link" to="Footer" spy={true} smooth={true} offset={-350} duration={500} >About us</Link>
             </li>
           </ul>
           <button className="mybtn" type="submit">LOGIN</button>
